ddeac13d03c1f319950655485e086982acfa2940
Daniel Verevkin <werowkin@gmail.com>
2024-08-21 18:46:09 +0200

    camera: Init service (create logger, capture camera, start listening)

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.22
 
 require (
 	github.com/blackjack/webcam v0.6.1 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/sys v0.14.0 // indirect
 )
--- internal/cam/cam.go
+++ internal/cam/cam.go
@@ -1,49 +1,143 @@
 package cam
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"flag"
 	"fmt"
 	"github.com/blackjack/webcam"
+	"go.uber.org/zap"
+	"os"
+	"path/filepath"
 )
 
+const serviceName = "cam"
+
+type Config struct {
+	ServerAddress string `json:"server_address"`
+	CamName       string `json:"cam_name"`
+	CamSource     string `json:"cam_source"`
+	Token         string `json:"token"`
+}
+
+type Cam struct {
+	webcam *webcam.Webcam
+	logger *zap.Logger
+	config Config
+}
+
+func (c *Cam) ListenAndServe() error {
+	c.logger.Info("Start camera listening", zap.String("cam_name", c.config.CamName), zap.String("server_address", c.config.ServerAddress))
+
+	return nil
+}
+
+func buildLogger() (*zap.Logger, error) {
+	cfg := zap.NewProductionConfig()
+	cfg.OutputPaths = []string{
+		filepath.Join("/var/log/", serviceName, "debug.log"),
+	}
+
+	cfg.ErrorOutputPaths = []string{
+		filepath.Join("/var/log/", serviceName, "error.log"),
+	}
+
+	cfg.Level = zap.NewAtomicLevelAt(zap.DebugLevel)
+
+	logger, err := cfg.Build()
+	if err != nil {
+		return nil, fmt.Errorf("cannot build logger: %w", err)
+	}
+
+	return logger, nil
+}
+
 func Run() error {
-	cam, err := captureCam()
+	cfg, err := readConfig()
+	if err != nil {
+		return fmt.Errorf("cannot read config: %w", err)
+	}
+
+	c, err := captureCam(cfg.CamSource)
 	if err != nil {
 		return fmt.Errorf("cannot capture cam: %w", err)
 	}
 
-	defer cam.Close()
-
-	err = cam.StartStreaming()
+	logger, err := buildLogger()
 	if err != nil {
-		return fmt.Errorf("failed to start streaming: %v", err)
+		return fmt.Errorf("cannot build logger: %w", err)
 	}
 
-	for {
-		err = cam.WaitForFrame(1)
-		switch err.(type) {
-		case nil:
-		case *webcam.Timeout:
-			continue
-		default:
-			return fmt.Errorf("failed to wait for frame: %v", err)
-		}
-
-		frame, err := cam.ReadFrame()
-		if err != nil {
-			return fmt.Errorf("failed to read frame: %v", err)
-		}
-
-		fmt.Println(len(frame))
+	cam := Cam{logger: logger, webcam: c, config: cfg}
+	if err = cam.ListenAndServe(); err != nil {
+		return fmt.Errorf("cannot start listening cam: %w", err)
 	}
+
+	err = c.Close()
+	if err != nil {
+		logger.Error("cannot close cam", zap.Error(err))
+	}
+
+	err = logger.Sync()
+	if err != nil {
+		return fmt.Errorf("cannot sync logger: %w", err)
+	}
+
+	return nil
 }
 
-func captureCam() (*webcam.Webcam, error) {
-	// todo: choose cam dialog
-	camPath := "/dev/video0"
-	cam, err := webcam.Open(camPath)
+func readConfig() (Config, error) {
+	cfgFile, err := os.Open(filepath.Join("/etc", serviceName, "config.json"))
+	if !errors.Is(os.ErrNotExist, err) {
+		return Config{}, fmt.Errorf("config file error (/etc/cam/config.json) while read config: %w", err)
+	}
+
+	b := bytes.Buffer{}
+	n, err := b.ReadFrom(cfgFile)
+	if err != nil {
+		return Config{}, fmt.Errorf("config file error (/etc/cam/config.json) while read config content: %w", err)
+	}
+
+	var cfg Config
+	if n > 0 {
+		err = json.Unmarshal(b.Bytes(), &cfg)
+		if err != nil {
+			return Config{}, fmt.Errorf("config file error (/etc/cam/config.json) while unmarshall config content: %w", err)
+		}
+	}
+
+	serverAddress := flag.String("server", cfg.ServerAddress, "server address")
+	token := flag.String("token", cfg.Token, "token")
+	name := flag.String("name", cfg.CamName, "camera name")
+	src := flag.String("src", cfg.CamSource, "camera source")
+	flag.Parse()
+
+	cfg.ServerAddress = *serverAddress
+	cfg.Token = *token
+	cfg.CamName = *name
+	cfg.CamSource = *src
+
+	if cfg.CamName == "" {
+		return Config{}, fmt.Errorf("cam name is required for identificate camera on server")
+	}
+
+	if cfg.ServerAddress == "" {
+		return Config{}, fmt.Errorf("server address is required for communicating with server")
+	}
+
+	if cfg.Token == "" {
+		return Config{}, fmt.Errorf("token is required for authorize camera on server")
+	}
+
+	return cfg, nil
+}
+
+func captureCam(path string) (*webcam.Webcam, error) {
+	cam, err := webcam.Open(path)
 
 	if err != nil {
-		return nil, fmt.Errorf("failed to open camera by path %s: %w", camPath, err)
+		return nil, fmt.Errorf("failed to open camera by path %s: %w", path, err)
 	}
 
 	return cam, nil
